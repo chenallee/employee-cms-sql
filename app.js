a13baf39331a04eee28bbad80acca24f2e52e048
chenallee <lchenalloy@gmail.com>
2020-02-26 14:27:28 -0500

    view all sql query working

--- app.js
+++ app.js
@@ -5,16 +5,36 @@ require('console.table');
 //import connection
 const connection = require('./config/connection');
 
+//
+const { viewAllEmp } = require('./lib/db-query');
+
+//import questions
+const menuPrompt = require('./lib/prompt');
+const { employeesPrompt, empViewOptions } = require('./lib/employee-prompt');
 
 
 //welcome user with initial screen
   //call entry prompt function
 
-//entry prompt function:
-  //have user select if they want to interact with Employees / Roles / Departments
-    //if Employees call Employees menu 
-    //if Roles call Roles menu
+/*entry prompt function:
+  Have user select if they want to interact with Employees / Roles / Departments
+    --> if Employees call Employees menu 
+    --> if Roles call Roles menu 
+    --> if Departments call Departments menu */
+const entryPrompt = async () => {
 
+  const { selected_menu: menuSelect } = await inquirer.prompt(menuPrompt);
+
+  if (menuSelect === 'Employees'){
+    employeesMenu();
+  } else if (menuSelect === 'Roles'){
+    rolesMenu();
+  } else if (menuSelect === 'Departments'){
+    departmentsMenu();
+  } else {
+    connection.end();
+  }
+;}
 
 //call Employees prompt which asks user if they want to View/Update/Add/Delete
   //if View call View menu
@@ -23,6 +43,34 @@ const connection = require('./config/connection');
     //Update menu asks if user wants to Update Role / Update Manager
   //if Add call Add E prompts (name/surname/role from list/manager from list)*
   //if Delete call Delete E prompts
+const employeesMenu = async () => {
+  //console.log('employees');
+  const { employees_menu: employeesAction } = await inquirer.prompt(employeesPrompt);
+
+  if (employeesAction === 'View Employees'){
+    viewEmpMenu();
+  } else if (employeesAction === 'Update Employee'){
+    updateEmpMenu();
+  } else if (employeesAction === 'Add Employee'){
+    addEmpMenu();
+  } else if (employeesAction === 'Delete Employee'){
+    delEmpMenu();
+  }
+};
+
+//View menu asks if user wants to View All E / View E by Role / View E by Manager/ (View E by Dpt)
+const viewEmpMenu = async() => {
+  console.log('view');
+  const { employees_view: empViewSelect } = await inquirer.prompt(empViewOptions);
+
+  if (empViewSelect === 'View All'){
+    const allEmps = await viewAllEmp();
+    console.table(allEmps);
+  } else if (empViewSelect === 'View by Role'){
+    const rolesEmp =
+  }
+
+};
 
 //call Roles prompt which asks user if they want to View role/Add role/Delete role
   // if View -> show all roles -> call Entry Prompt
@@ -35,10 +83,11 @@ const connection = require('./config/connection');
   // if Add call Add D prompts (name)
   // if Delete call Delete R prompts (list)
 
-// *** uh do we have a separate manager DB? that allows user to select maanager or...***
+
 
 //connect to the database
 connection.connect(err => {
   if (err) throw err;
   console.log('connected to DB');
+  entryPrompt();
 })
--- config/connection.js
+++ config/connection.js
@@ -19,22 +19,21 @@ module.exports = connection;
 
 //RETURNING FULL TABLE OF EMPLOYEES IN MYSQL
 /** 
--- USE company_cms;
--- SELECT 
--- 	e1.id, e1.first_name, e1.last_name, role.title, department.name, role.salary, concat(b1.first_name, ' ', b1.last_name) AS manager
--- FROM 
--- 	employees AS e1
--- LEFT JOIN 
--- 	role 
--- ON 
--- 	e1.role_id = role.id
--- LEFT JOIN 
--- 	department
--- ON
--- 	role.department_id = department.id
--- LEFT JOIN
--- 	employees AS b1
--- ON
--- 	e1.manager_id = b1.id;
--- ;
+USE company_cms;
+SELECT 
+ 	e1.id, e1.first_name, e1.last_name, role.title, department.name, role.salary, concat(b1.first_name, ' ', b1.last_name) AS manager
+ FROM 
+ 	employees AS e1
+ LEFT JOIN 
+ 	role 
+ ON 
+ 	e1.role_id = role.id
+ LEFT JOIN 
+ 	department
+ ON
+ 	role.department_id = department.id
+ LEFT JOIN
+ 	employees AS b1
+ ON
+ 	e1.manager_id = b1.id;
 **/
